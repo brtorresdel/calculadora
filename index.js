5b06ce47b9ed35e8bdb8db492a51da002a1d53bc
Bruno Torres <br.torres.del@gmail.com>
2024-11-09 23:43:23 +0000

    Mais CSS e iniciada as funções de cálculo (ainda não ta pronto!)

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
 let botoes = document.querySelectorAll('.botao');
 let divCalculo = document.querySelector('#calculo');
 let divResultado = document.querySelector('#resultado');
+let a = '';
+let b = '';
+let op = '';
 
 botoes.forEach(botao => {
     botao.addEventListener('click', () => {
         switch(botao.value) {
             case "C":
-                divCalculo.textContent = '';
-                divResultado.textContent = '';
+                divCalculo.innerHTML = '0';
+                divResultado.innerHTML = '0';
+                divResultado.classList.add('vazio');
+                a = '';
+                b = '';
+                break;
+
+            case "=":
+                if (op !== '') {
+                    divResultado.innerHTML = operacao(a,b,op);
+                    divResultado.classList.remove('vazio');
+                    divCalculo.innerHTML = 0;
+                }
                 break;
 
             default:
-                divCalculo.textContent = botao.value;
-                divResultado.textContent = botao.value;
-                break;
+                addChar(botao.value);
         }
     })
 })
 
-function operacao(a, b, op) {
+function operacao(n1, n2, op) {
     switch (op) {
         case '+':
-            return a + b;
+            return n1 + n2;
         case '-':
-            return a - b;
+            return n1 - n2;
         case '*':
-            return a * b;
+            return n1 * n2;
         case '/':
-            return a / b;
+            return n1 / n2;
     }
+}
+
+function addChar(n) {
+  if (divCalculo.innerHTML === '0') {
+    divCalculo.innerHTML = n;
+    return;
+  } else {
+    if (/[+\-*/]/.test(n)) {
+      if (!/[+\-*/]/.test(divCalculo.innerHTML)) {
+        divCalculo.innerHTML += n;
+        op = n;
+        return;
+      }
+    } else {
+      divCalculo.innerHTML += n;
+      if (!/[+\-*/]/.test(divCalculo.innerHTML)){a = a + n.toString()}
+      else {b = b + n.toString()}
+    }
+  }
+  console.log(a);
+  console.log(b);
+  console.log(op);
 }
